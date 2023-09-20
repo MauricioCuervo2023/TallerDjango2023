f842c7112a44ea6cb8a343447b83772bdd3792b9
MauricioCuervo2023 <127361506+MauricioCuervo2023@users.noreply.github.com>
2023-09-19 22:05:55 -0500

    primer template funcionando ok

--- principal/views.py
+++ principal/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 def inicio(request):
-    context={} 
+    titulo = "Inicio"
+    nombre = "Mauricio Cuervo"
+    context={
+        "titulo": titulo,
+        "nombre": nombre,
+    } 
     return render(request, 'index.html', context)
